589b373c98ff0b03e1858391414ed7a0bf68249c
Kaue Braga <kauebraga90@gmail.com>
2021-06-28 16:58:27 -0300

    update

--- R/3.2-calculate_ttmatrix.R
+++ R/3.2-calculate_ttmatrix.R
@@ -13,37 +13,48 @@ calculate_ttmatrix <- function(sigla_muni) {
   points <- fread(sprintf("../../data/avaliacao_intervencoes/r5/points/points_%s_09_2019.csv", sigla_muni)) %>% select(id = id_hex, lat = Y, lon = X)
   mode <- c("WALK", "TRANSIT")
   max_walk_dist <- 3000   # meters
-  max_trip_duration <- 120 # minutes
+  max_trip_duration <- 180 # minutes
   departure_datetime <- as.POSIXct("02-03-2020 06:00:00", format = "%d-%m-%Y %H:%M:%S")
   
   r5r_core <- setup_r5(data_path = sprintf("../../data/avaliacao_intervencoes/r5/graph/%s_antes/", sigla_muni), verbose = FALSE)
   
   # 3.1) calculate a travel time matrix
   ttm1 <- travel_time_matrix(r5r_core = r5r_core,
-                            origins = points,
-                            destinations = points,
-                            mode = mode,
-                            departure_datetime = departure_datetime,
-                            time_window = 2,
-                            max_walk_dist = max_walk_dist,
-                            max_trip_duration = max_trip_duration)
+                             origins = points,
+                             destinations = points,
+                             mode = mode,
+                             departure_datetime = departure_datetime,
+                             time_window = 2,
+                             max_walk_dist = max_walk_dist,
+                             max_trip_duration = max_trip_duration)
+  # classify
+  ttm1[, type := "before_intervention"]
   
   
   r5r_core <- setup_r5(data_path = sprintf("../../data/avaliacao_intervencoes/r5/graph/%s_depois/", sigla_muni), verbose = TRUE)
   
   ttm2 <- travel_time_matrix(r5r_core = r5r_core,
-                            origins = points,
-                            destinations = points,
-                            mode = mode,
-                            departure_datetime = departure_datetime,
-                            time_window = 2,
-                            max_walk_dist = max_walk_dist,
-                            max_trip_duration = max_trip_duration)
+                             origins = points,
+                             destinations = points,
+                             mode = mode,
+                             departure_datetime = departure_datetime,
+                             time_window = 2,
+                             max_walk_dist = max_walk_dist,
+                             max_trip_duration = max_trip_duration)
+  
+  # classify
+  ttm1[, type := "after_intervention"]
   
   ttm <- full_join(ttm1, ttm2,
                    by = c("fromId", "toId"),
                    suffix = c("_antes", "_depois"))
   
-  setDT(ttm)[, dif := travel_time_antes - travel_time_depois]
-
-  }
+  # rename
+  ttm <- ttm %>% rename(origin = fromId, destination = toId)
+  
+  
+  
+  # save
+  readr::write_rds(ttm, sprintf("../../data/avaliacao_intervencoes/ttmatrix/ttmatrix_%s.rds", sigla_muni))
+  
+}
